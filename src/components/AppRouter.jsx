c7fd72dfbf16ccb141b804b786206b84c16c5a13
iglbv <ilyagolubev2021@gmail.com>
2026-01-24 22:39:19 +0500

    optimized router

--- src/components/AppRouter.jsx
+++ src/components/AppRouter.jsx
@@ -1,18 +1,17 @@
 import React from 'react';
-import { Routes, Route, Navigate } from 'react-router-dom';
-import Posts from '../pages/Posts';
-import About from '../pages/About';
-import Error from '../pages/Error';
-import PostIdPage from '../pages/PostIdPage';
+import { Routes, Route } from 'react-router-dom';
+import { routes } from '../router/routes';
 
 const AppRouter = () => {
     return (
         <Routes>
-            <Route path="/posts" element={<Posts />} />
-            <Route path="/about" element={<About />} />
-            <Route path="posts/:id" element={<PostIdPage />} />
-            <Route path="/error" element={<Error />} />
-            <Route path="*" element={<Navigate to="/error" replace />} />
+            {routes.map(route => (
+                <Route
+                    key={route.path}
+                    path={route.path}
+                    element={route.element}
+                />
+            ))}
         </Routes>
     )
 }
